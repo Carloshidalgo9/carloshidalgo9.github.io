83e3d7f3c28f79863508496e6594b0c1ba61cc82
carloshidalgo9 <carlosht717@gmail.com>
2024-03-07 22:39:24 -0600

    curriculum1

---
+++ index.js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector (".toggle")
+const navmenu = document.querySelector(".nav-memu")  
+toggle.addEventListener("click", ()=>{ 
+ navMenu.classList.toggle("nav-menvi");
+}); 
